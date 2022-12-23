a3fa0188dfc138a381c2a661f9a56b237d633081
Don Function <69618397+MRFUNCT1ON@users.noreply.github.com>
2022-12-23 22:46:27 +1000

    Initial Commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x2C3408a4827DF0419DA2f53eAe92f338B4d314ec'
+
+export const INIT_CODE_HASH = '0x49fce07dac7533431899a4f0b442a33e33814f7456d8761e6e916cb6d9a1ddc0'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
